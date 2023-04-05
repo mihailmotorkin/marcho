4d9ceee8abf235ee71b330853d0ec21564868981
mihailmotorkin <motorkinmihail@gmail.com>
2023-04-05 20:07:48 +0530

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -14,6 +14,35 @@ $(function (){
     readOnly: true
   });
  
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    }
+  });
+
+  $('.select-style').styler();
+
+  $('.shop-content__filter-btn').on('click', function () {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active'); 
+    $(this).addClass('shop-content__filter-btn--active');
+  })
+
+  $('.button-list').on('click', function () {
+    $('.product-item').addClass('product-item--list');
+  })
+
+  $('.button-grid').on('click', function () {
+    $('.product-item').removeClass('product-item--list');
+  })
+
+
 
   function getTimeRemaining(endtime) {
   const total = Date.parse(endtime) - Date.parse(new Date());
